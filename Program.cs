0878b48e6852d1c5731982c931a39f4ee083872e
Kedar Brooks <kedarbrooks777@outlook.com>
2016-04-25 20:01:14 -0500

    inital commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {   
+        public static void Main(string[] args)
+        {
+            var shell = "#->"; 
+            Console.WriteLine("FRC-DataCompiler 1.0");
+            Console.WriteLine("X-shell version 0.7");
+            Console.WriteLine(shell); 
+            Console.ReadLine();    
+        }
+        public static void CLSparsing 
+        {
+            
+        }
+    }
+}

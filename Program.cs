04180ebde61b01c002b30aa3936d9f30a7a15178
Kedar Brooks <kedarbrooks777@outlook.com>
2016-04-27 18:26:44 -0500

    base commit

--- Program.cs
+++ Program.cs
@@ -4,17 +4,27 @@ namespace ConsoleApplication
 {
     public class Program
     {   
+	  
         public static void Main(string[] args)
         {
-            var shell = "#->"; 
+            var shell = "#->";
+            string shellInput; 
+  
             Console.WriteLine("FRC-DataCompiler 1.0");
-            Console.WriteLine("X-shell version 0.7");
-            Console.WriteLine(shell); 
-            Console.ReadLine();    
+	    Console.WriteLine("X Shell Version 0.7"); 
+            shellInput = Console.ReadLine();
+            Commander(shellInput); 
         }
-        public static void CLSparsing 
+        public static void Commander(string Input)
         {
-            
+            if (Input == "report")
+            {
+                Console.WriteLine("Reporting..."); 
+            }
+            else 
+            {
+            }
         }
     }
-}
+
+  }
